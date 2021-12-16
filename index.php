40ce93e8ab4f927d3761d23ead9c9c28eb3a61f4
simone.eufemi <s1m1eufo@gmail.com>
2021-12-17 00:05:33 +0100

    initial

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
-require 'simple_html_dom.php';
+require 'library/simple_html_dom.php';
 
-var_dump("TEST");
+$html = file_get_html('http://www.google.com/');
+
+// Find all images
+foreach ($html->find('img') as $element)
+    echo $element->src . '<br>';
+
+// Find all links
+foreach ($html->find('a') as $element)
+    echo $element->href . '<br>';
