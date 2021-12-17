684f2a51355d6aedbf12e75e477e87d9b1e25f5f
simone.eufemi <s1m1eufo@gmail.com>
2021-12-17 21:37:41 +0100

    initial

--- index.php
+++ index.php
@@ -9,18 +9,3 @@ $html = file_get_html($urlFinal);
 
 
 $agent = 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.0.0 Mobile Safari/537.36';
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_VERBOSE, true);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-curl_setopt($ch, CURLOPT_URL, $urlFinal);
-$result = curl_exec($ch);
-
-$myfile = fopen("html.txt", "w") or die("Unable to open file");
-fwrite($myfile, $result);
-
-fclose($myfile);
-
-var_dump("ok");
