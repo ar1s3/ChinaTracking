3ca5024031494c725793f55cf94ffd82956f55d9
simone.eufemi <s1m1eufo@gmail.com>
2021-12-18 00:30:13 +0100

    initial

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
+//use library\Parsehub;
 
 require_once('library/utils.php');
+require_once('library/Parsehub/Parsehub.php');
 
 $urlBase = "https://www.saramart.pl/it-IT/detail/";
 $urlProductDetail = "27639316";
 $urlFinal = $urlBase . $urlProductDetail;
+
 $html = file_get_html($urlFinal);
-
-
 $agent = 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.0.0 Mobile Safari/537.36';
+
+
+
