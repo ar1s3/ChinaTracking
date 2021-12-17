47966940f4da52b84291c6492fe753a66a77745c
simone.eufemi <s1m1eufo@gmail.com>
2021-12-18 00:46:37 +0100

    initial

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-//use library\Parsehub;
+
+
+//use library\utils;
 
 require_once('library/utils.php');
 require_once('library/Parsehub/Parsehub.php');
@@ -11,5 +13,23 @@ $urlFinal = $urlBase . $urlProductDetail;
 $html = file_get_html($urlFinal);
 $agent = 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.0.0 Mobile Safari/537.36';
 
+$utils = new utils();
 
+// echo $utils->runParser($urlFinal);
 
+$params = http_build_query(array(
+    "api_key" => MY_API_KEY,
+    "offset" => "0",
+    "include_options" => "1"
+));
+
+$result = file_get_contents(
+    'https://parsehub.com/api/v2/projects/ty88FLuX1Gta?'.$params,
+    false,
+    stream_context_create(array(
+        'http' => array(
+            'method' => 'GET'
+        )
+    ))
+);
+var_dump($result);
--- library/utils.php
+++ library/utils.php
@@ -1,4 +1,8 @@
 <?php
+//VAR_DUMP LIMITS
+ini_set('xdebug.var_display_max_depth', -1); //10
+ini_set('xdebug.var_display_max_children', -1); //256
+ini_set('xdebug.var_display_max_data', -1); //1024
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
@@ -25,7 +29,7 @@ class utils
         return $data;
     }
 
-    function runParser($url)
+    public function runParser($url)
     {
 
         $params = array(
@@ -47,7 +51,7 @@ class utils
         $context = stream_context_create($options);
         $return = file_get_contents('https://parsehub.com/api/v2/projects/ty88FLuX1Gta/run', false, $context);
 
-        return !empty($return) ?? "Error";
+        return !empty($return) ? "OK" : "Error";
 
     }
 
