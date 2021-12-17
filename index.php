0074af103f0a24d0765a81c45a6258009335d851
simone.eufemi <s1m1eufo@gmail.com>
2021-12-17 21:09:59 +0100

    initial

--- index.php
+++ index.php
@@ -9,12 +9,17 @@ $urlFinal = $urlBase . $urlProductDetail;
 
 //titolo articolo magari echo file_get_html($urlFinal)->plaintext;
 
-
-$f = file_get_contents_curl($urlFinal);
-
+/*
+$a = new utils();
+$a->file_get_contents_curl("google.it");
 $dom = new DOMDocument();
-@$dom->loadHTML($f);
+@$dom->loadHTML($a);
 $data = $dom->getElementById("app");
 $html = $dom->saveHTML($data);
 echo $html;
+*/
+
+$a = file_get_contents("https://www.google.it");
+echo "<pre>";
+var_dump($a);
 
