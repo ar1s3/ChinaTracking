14f7cfd9aced95f03cdd9523889cf31e6f6ad8d8
simone.eufemi <s1m1eufo@gmail.com>
2021-12-18 12:05:38 +0100

    initial

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-
-require_once __DIR__ . '/vendor/autoload.php';
 require_once('library/utils.php');
 
-use Parsehub\Parsehub;
 
 
 $urlBase = "https://www.saramart.pl/it-IT/detail/";
@@ -13,8 +10,10 @@ $urlFinal = $urlBase . $urlProductDetail;
 $html = file_get_html($urlFinal);
 $agent = 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.0.0 Mobile Safari/537.36';
 
-$api_key = MY_API_KEY;
-$parsehub = new Parsehub($api_key);
-$projectList = $parsehub->getProjectList();
-var_dump($projectList);
+$util = new utils();
+
+echo($util->getPrjList());
+
+
+
 
--- library/utils.php
+++ library/utils.php
@@ -8,15 +8,16 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+require_once __DIR__ . '/../vendor/autoload.php';
 require('simple_html_dom.php');
 
+use Parsehub\Parsehub;
 const MY_API_KEY = "tTXn1ETK9h_V";
 
 class utils
 {
 
-    function file_get_contents_curl($url)
-    {
+    function file_get_contents_curl($url){
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -28,5 +29,12 @@ class utils
         curl_close($ch);
 
         return $data;
+    }
+
+    public function getPrjList($api_key = MY_API_KEY){
+        $parsehub = new Parsehub($api_key);
+        $projectList = $parsehub->getProjectList();
+        echo "<pre>";
+        var_dump($projectList);
     }
 }
