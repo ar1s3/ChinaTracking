cd11ec6c25f95e13ed19f60b3d29e34af6c76e41
simone.eufemi <s1m1eufo@gmail.com>
2021-12-19 02:39:08 +0100

    init2

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+//VAR_DUMP LIMITS
+ini_set('xdebug.var_display_max_depth', -1); //10
+ini_set('xdebug.var_display_max_children', -1); //256
+ini_set('xdebug.var_display_max_data', -1); //1024
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 
 require_once __DIR__ . '/vendor/autoload.php';
 
