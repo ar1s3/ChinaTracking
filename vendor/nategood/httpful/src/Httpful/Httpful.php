203f0369d3041857d0c666a4a195a3bf8114fba7
simone.eufemi <s1m1eufo@gmail.com>
2021-12-19 02:25:09 +0100

    init2

---
+++ vendor/nategood/httpful/src/Httpful/Httpful.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Httpful;
+
+use Httpful\Handlers\MimeHandlerAdapter;
+
+class Httpful {
+    const VERSION = '0.2.20';
+
+    private static $mimeRegistrar = array();
+    private static $default = null;
+
+    /**
+     * @param string $mimeType
+     * @param MimeHandlerAdapter $handler
+     */
+    public static function register($mimeType, MimeHandlerAdapter $handler) {
+        self::$mimeRegistrar[$mimeType] = $handler;
+    }
+
+    /**
+     * @param string $mimeType defaults to MimeHandlerAdapter
+     * @return MimeHandlerAdapter
+     */
+    public static function get($mimeType = null) {
+        if (isset(self::$mimeRegistrar[$mimeType])) {
+            return self::$mimeRegistrar[$mimeType];
+        }
+
+        if (empty(self::$default)) {
+            self::$default = new MimeHandlerAdapter();
+        }
+
+        return self::$default;
+    }
+
+    /**
+     * Does this particular Mime Type have a parser registered
+     * for it?
+     * @param string $mimeType
+     * @return bool
+     */
+    public static function hasParserRegistered($mimeType) {
+        return isset(self::$mimeRegistrar[$mimeType]);
+    }
+}
