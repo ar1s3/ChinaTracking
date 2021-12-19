07e3180aa7656287c867995a6ef62a4aed1986ad
simone.eufemi <s1m1eufo@gmail.com>
2021-12-19 20:05:11 +0100

    init2

--- library/utils.php
+++ library/utils.php
@@ -21,4 +21,39 @@ const MY_PRJ_KEY = "ty88FLuX1Gta";
 
 class utils {
 
+    public function getPrjList($api_key = MY_API_KEY) {
+        $parsehub = new Parsehub($api_key);
+        $projectList = $parsehub->getProjectList();
+        echo "<pre>";
+        var_dump($projectList);
+    }
+
+    public function getDataPrj($api_key = MY_API_KEY, $prj_key = MY_PRJ_KEY): string {
+
+        $parsehub = new Parsehub($api_key);
+        return $parsehub->getLastReadyRunData($prj_key);
+
+    }
+
+    public function customGetDataPrj() {}
+
+    public function runPrj(array $keywords, $api_key = MY_API_KEY, $prj_key = MY_PRJ_KEY) {
+        if (empty($keywords)) {
+            return 0;
+        }
+
+        $parsehub = new Parsehub($api_key);
+        $options = array(
+            // Skip start_url option if don't want to override starting url configured
+            // on parsehub.
+            'start_url' => 'https://www.saramart.pl/it-IT/s/',
+            // Enter comma separated list of keywords to pass into `start_value_override`
+            'keywords' => implode($keywords),
+            // Set send_email options. Skip to remain this value default.
+            'send_email' => 0
+        );
+        $run_obj = $parsehub->runProject($prj_key, $options);
+        var_dump($run_obj);
+    }
+
 }
