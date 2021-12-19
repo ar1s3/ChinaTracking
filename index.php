f86a64181a2bf94dc23fd6b0bb825cb5e91c5962
simone.eufemi <s1m1eufo@gmail.com>
2021-12-19 02:37:57 +0100

    init2

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "hIOUGP";
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Parsehub\Parsehub;
+
+$api_key = "tTXn1ETK9h_V";
+$parsehub = new Parsehub($api_key);
+$projectList = $parsehub->getProjectList();
+print $projectList;
+
