adba8d13cc05dc9e7c8dddb0084af9f534868910
simone.eufemi <s1m1eufo@gmail.com>
2021-12-19 02:49:18 +0100

    init2

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 //VAR_DUMP LIMITS
-ini_set('xdebug.var_display_max_depth', -1); //10
-ini_set('xdebug.var_display_max_children', -1); //256
-ini_set('xdebug.var_display_max_data', -1); //1024
+$ch = curl_init();
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
