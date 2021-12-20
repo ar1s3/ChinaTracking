348cc75a99956d25aef1a8ffb6638bc0d0c31e55
simone.eufemi <s1m1eufo@gmail.com>
2021-12-21 00:26:58 +0100

    init2

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
-
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+require_once('library/utils.php');
+
+$parse = new utils();
+
+$keys = array("moncler");
+
+$res = $parse->runPrj($keys);
+
+var_dump($res);
 
--- library/utils.php
+++ library/utils.php
@@ -12,9 +12,6 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require('simple_html_dom.php');
-
-use Parsehub\Parsehub;
 
 const MY_API_KEY = "tTXn1ETK9h_V";
 const MY_PRJ_KEY = "ty88FLuX1Gta";
