a5c9faf807fba4cc44021f23514324de01b11508
simone.eufemi <s1m1eufo@gmail.com>
2021-12-26 23:42:17 +0100

    init2

--- index.php
+++ index.php
@@ -8,23 +8,10 @@ require_once('library/saramart.php');
 
 $parse = new utils();
 $saraObj = new saramart();
-//$datas = json_decode($saraObj->getDataPrj());
+$datas = json_decode($saraObj->getDataPrj());
 
 echo "<pre>";
 
-//var_dump($datas);
-/*
-foreach ($datas->type[0]->selection3 as $key => $data) {
-
-    //echo "<img src='".$data->image."'</img><br>";
-    if (strpos($data->image, "data:image/svg+xml") === false) {
-        echo $data->image . "<br>";
-    }
-
-}  */
-
-//echo $saraObj->runPrj("moncler");
-
-print($saraObj->getDataPrj());
+print($datas);
 
 
