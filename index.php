93ecd58249db77c826ea0c8f76a87d0707f9b48a
simone.eufemi <simone.eufemi@eng.it>
2021-12-27 14:45:33 +0100

    init2-work

--- index.php
+++ index.php
@@ -60,13 +60,14 @@ foreach ($details as $dets){
 <section class="py-5">
     <div class="container px-4 px-lg-5 mt-5">
         <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            <div class="col mb-5">
+
 
                 <?php
 
                     foreach ($details as $dets){
 
                 ?>
+                <div class="col mb-5">
                 <div class="card h-100"> <!-- SCHEDA ITEM -->
 
                     <!-- Sale badge
