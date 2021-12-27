ecbddc103644eb33cb50160fc76440f681d5c010
simone.eufemi <simone.eufemi@eng.it>
2021-12-27 12:09:27 +0100

    init2-work

--- index.php
+++ index.php
@@ -29,11 +29,11 @@ $desc_item = array_map("unserialize", array_unique(array_map("serialize", $desc_
 //print_r($details);
 //echo $util->implode_all("\n", $desc_item);
 
-foreach ($desc_item as &$desc) {
+foreach ($details as &$det) {
 
     //foreach ($details as $det) {
 
-        $desc['pippo']= 'test';
+        $det['pippo']= 'test';
 
     //}
 
