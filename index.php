d4ecad36c31bbf3a62a8ff279e8a02c05d4048cd
simone.eufemi <s1m1eufo@gmail.com>
2022-01-10 20:54:58 +0100

    init2

--- index.php
+++ index.php
@@ -48,6 +48,16 @@ $cleanedArray = $util->deDuplicate($cleanedArray);
         });
     </script>
 </head>
+
+<header class="bg-dark py-5">
+    <div class="container px-4 px-lg-5 my-5">
+        <div class="text-center text-white">
+            <h1 class="display-4 fw-bolder">TEST N1</h1>
+            <p class="lead fw-normal text-white-50 mb-0">CAPTION N1</p>
+        </div>
+    </div>
+</header>
+
 <body>
 <div class="m-4">
     <ul class="nav nav-tabs" id="myTab">
