4542cd76b47bcf40940782fd8a74580480304200
simone.eufemi <s1m1eufo@gmail.com>
2022-01-10 19:51:02 +0100

    init2

--- index.php
+++ index.php
@@ -36,20 +36,19 @@ $cleanedArray = $util->deDuplicate($cleanedArray);
 <!-- HTML BEGIN -->
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content=""/>
     <meta name="author" content=""/>
     <title>Shop Homepage - Start Bootstrap Template</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"/>
-
-    <link href="css/styles.css" rel="stylesheet"/>
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 </head>
+
 
 <script>
     function openCity(evt, cityName) {
